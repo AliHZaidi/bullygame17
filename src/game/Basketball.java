fe314f366d60814b965d39aa9ce17c4ef5897470
Matthew Kramer <kramer22@wisc.edu>
2017-02-19 09:45:21 -0600

    Fixed basketball happiness bug and generalized getPrompt

--- src/game/Basketball.java
+++ src/game/Basketball.java
@@ -22,10 +22,14 @@ public class Basketball extends NonPlayableObject {
 		
 	}
 	
-	public Dialogue getPrompt(Character x){
+	public Dialogue getPrompt(Character x, boolean shudChange){
 		int amount = 20 -i;
-		x.increaseHappiness(amount);
-		i -= 5;
+		if(shudChange){
+			
+			x.increaseHappiness(amount);
+			if(i < 20)
+				i += 5;
+		}
 		return new Dialogue("You play some basketball, but after playing it seems more boring"
 				+ "\nYour Happiness Increases by: " + amount);
 	}
