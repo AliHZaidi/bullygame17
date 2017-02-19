1f2a1bca83e119d9d55d030132fae52a6a7c456b
Cal Rueb <calvin.c.rueb@gmail.com>
2017-02-19 09:31:53 -0600

    Added walls to outdoor and friends home

--- src/locations/FriendHome.java
+++ src/locations/FriendHome.java
@@ -14,15 +14,25 @@ public class FriendHome extends Location {
     public FriendHome() {
     	tv = new Television("Television.png",250,200);
         this.map = new ArrayList<NonPlayableObject>();
+        ImageIcon homeWall = new ImageIcon("box.png");
         for(int i = 0; i < 20; i++) {
-            super.map.add(new Box(new ImageIcon("box.png").getImage(),i, 19));
+            super.map.add(new Box(homeWall.getImage(),i, 19));
         }
         for(int i = 1; i < 20; i++) {
-            super.map.add(new Box(new ImageIcon("box.png").getImage(),0, i));
+            super.map.add(new Box(homeWall.getImage(),0, i));
         }
         for(int i = 1; i < 20; i++) {
-            super.map.add(new Box(new ImageIcon("box.png").getImage(),19, i));
+            super.map.add(new Box(homeWall.getImage(),19, i));
         }
+
+        //top wall
+        for(int i = 0; i < 20; i++) {
+            if(i < 8 || i > 12) {
+                super.map.add(new Box(homeWall.getImage(), i, 0));
+            }
+            
+        }
+
         map.add(tv);
     }
     
